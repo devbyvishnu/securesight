460262d9e395cf6275989f4061aa98f8b3be6ffc
devbyvishnu <vishnu.zyberla@gmail.com>
2025-07-26 11:50:33 +0530

    ui updated

--- app/sections/Dashboard.jsx
+++ app/sections/Dashboard.jsx
@@ -4,7 +4,7 @@ import Incidentplayer from "../components/Incidentplayer";
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-gray-800 p-4 text-white">
-      <div className="flex gap-4 h-[420px]">
+      <div className="flex gap-4 h-[500px]">
         
         
         <div className="flex-[4] bg-gray-800 rounded-lg overflow-hidden">
@@ -15,7 +15,7 @@ const Dashboard = () => {
 
         
         <div className="flex-[3] bg-gray-800 rounded-lg overflow-hidden">
-          <div className="h-[410px] overflow-y-auto p-2 no-scrollbar">
+          <div className="h-[460px] overflow-y-auto p-2 no-scrollbar">
             <Incidentlist />
           </div>
         </div>
